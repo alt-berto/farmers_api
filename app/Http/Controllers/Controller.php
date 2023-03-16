63227d5d5d03c466c89541e92329827f58639048
Alberto Molina <5884429+alt-berto@users.noreply.github.com>
2023-03-15 22:22:06 -0600

    fix: user id

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -25,7 +25,7 @@ class Controller extends BaseController
     protected function invitation_mail(int $order_id): string {
         try {
             $order = Order::with(['details.inventory_price.inventory.product'])->where('state', 'done')->where('id', $order_id)->firstOrFail();
-            $user = User::where('client_id', $order->client_id)->firstOrFail();
+            $user = User::where('id', $order->client_id)->firstOrFail();
             $subject = "Notificación de canjeo de : $user->first_name $user->last_name - Orden #$order->id";
             $parameters = [
                 'user' => $user,
@@ -38,7 +38,7 @@ class Controller extends BaseController
                     //'aurora.matamoros@upl-ltd.com'
                 ]
             ];
-            
+
             Mail::send('mail.notification', $parameters, static function($message) use ($parameters) {
                 $message->to($parameters['to'], $parameters['to_name'])->subject($parameters['subject']);
                 $message->from(env('MAIL_FROM_CONTACT'), 'UPL FARMERS');
