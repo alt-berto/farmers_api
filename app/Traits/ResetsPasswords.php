93d076700953f7d37b63306b1a6afec098476f7c
Alberto Molina <5884429+alt-berto@users.noreply.github.com>
2023-03-15 21:59:45 -0600

    Update ResetsPasswords.php

--- app/Traits/ResetsPasswords.php
+++ app/Traits/ResetsPasswords.php
@@ -54,7 +54,6 @@ trait ResetsPasswords
         // If the password was successfully reset, we will redirect the user back to
         // the application's home authenticated view. If there is an error we can
         // redirect them back to where they came from with their error message.
-        return $response;
         return $response == Password::PASSWORD_RESET
             ? $this->sendResetResponse($request, $response)
             : $this->sendResetFailedResponse($request, $response);
