a969d7df52ea73061aa8803248db438850a2329f
Alberto Molina <5884429+alt-berto@users.noreply.github.com>
2023-03-15 22:21:22 -0600

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -23,24 +23,22 @@ class Controller extends BaseController
     //
 
     protected function invitation_mail(int $order_id): string {
-        $order = Order::with(['details.inventory_price.inventory.product'])->where('state', 1)->where('id', $order_id)->firstOrFail();
-        $user = User::where('client_id', $order->client_id)->firstOrFail();
-
-
-        $subject = "Notificación de canjeo de : $user->first_name $user->last_name - Orden #$order->id";
-        $parameters = [
-            'user' => $user,
-            'order' => $order,
-            'subject' => $subject,
-            'to' => (string) $user->email,
-            'to_name' => ($user->first_name . ' ' . $user->last_name),
-            'copies' => [
-                'ing.molinanestor@gmail.com',
-                //'aurora.matamoros@upl-ltd.com'
-                ]
-        ];
-
         try {
+            $order = Order::with(['details.inventory_price.inventory.product'])->where('state', 'done')->where('id', $order_id)->firstOrFail();
+            $user = User::where('client_id', $order->client_id)->firstOrFail();
+            $subject = "Notificación de canjeo de : $user->first_name $user->last_name - Orden #$order->id";
+            $parameters = [
+                'user' => $user,
+                'order' => $order,
+                'subject' => $subject,
+                'to' => (string) $user->email,
+                'to_name' => ($user->first_name . ' ' . $user->last_name),
+                'copies' => [
+                    'ing.molinanestor@gmail.com',
+                    //'aurora.matamoros@upl-ltd.com'
+                ]
+            ];
+            
             Mail::send('mail.notification', $parameters, static function($message) use ($parameters) {
                 $message->to($parameters['to'], $parameters['to_name'])->subject($parameters['subject']);
                 $message->from(env('MAIL_FROM_CONTACT'), 'UPL FARMERS');
